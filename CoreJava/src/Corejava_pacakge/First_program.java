3f06e15cccdb40eab755fc7054a6a25b66b6eae1
JBhavsar <JBhavsar@ATSINNB0626.ats-global.local>
2022-09-17 18:15:16 +0530

    First Commit

---
+++ CoreJava/src/Corejava_pacakge/First_program.java
@@ -0,0 +1,10 @@
+package Corejava_pacakge;
+
+public class First_program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println()
+	}
+
+}
---
+++ CoreJava/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author JBhavsar
+ *
+ */
+module CoreJava {
+}
